f809e673d08d6eab4a16b09a564b9eee883b6b80
OAT <teepakorn.oat@gmail.com>
2024-08-03 23:34:44 +0700

    Add Chiang Dao API

--- app.js
+++ app.js
@@ -13,6 +13,39 @@ app.get("/api", (req, res) => {
   const headers = {
     apikey: "DjZXKQP5F7vH9A8CSOHtVUB5trVeR9Un", 
   };
+  
+
+  // ส่ง Request ไปยัง URL ปลายทาง
+  fetch(url, { headers })
+    .then((response) => response.json())
+    .then((data) => res.json(data))
+    .catch((error) => res.status(500).send(error.message));
+});
+
+app.get("/Chom-Thong", (req, res) => {
+  // กำหนด URL ปลายทาง
+  const url =
+    "https://rguard.ntdigitalsolutions.com/api/province/Chiang Mai/district/Chom Thong/hourly-aqi";
+  const headers = {
+    apikey: "DjZXKQP5F7vH9A8CSOHtVUB5trVeR9Un", 
+  };
+  
+
+  // ส่ง Request ไปยัง URL ปลายทาง
+  fetch(url, { headers })
+    .then((response) => response.json())
+    .then((data) => res.json(data))
+    .catch((error) => res.status(500).send(error.message));
+});
+
+app.get("/Chiang-Dao", (req, res) => {
+  // กำหนด URL ปลายทาง
+  const url =
+    "https://rguard.ntdigitalsolutions.com/api/province/Chiang Mai/district/Chiang Dao/hourly-aqi";
+  const headers = {
+    apikey: "DjZXKQP5F7vH9A8CSOHtVUB5trVeR9Un", 
+  };
+  
 
   // ส่ง Request ไปยัง URL ปลายทาง
   fetch(url, { headers })
